450af08f97e410e5df1cabb427db01a9df1a810b
srkinator <srdjan@Srdjans-Mac-mini.local>
2018-05-18 16:32:53 +0200

    Sign in method implemented

--- server-side/controllers/authentication.js
+++ server-side/controllers/authentication.js
@@ -7,6 +7,10 @@ function tokenForUser(user) {
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+    res.send({ token: tokenForUser(req.user) });
+}
+
 exports.signup = function (req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
@@ -36,5 +40,4 @@ exports.signup = function (req, res, next) {
             res.json({ token: tokenForUser(user)});
         });
     });
-
 }
